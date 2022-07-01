034073e894a1611f0ffa5fef669db965da9fd05f
Frost <huuquangcap@gmail.com>
2022-07-01 12:48:27 -0400

    Added <plane-renderer>

--- src/components/Plane.js
+++ src/components/Plane.js
@@ -3,7 +3,9 @@ import PropTypes from "prop-types"
 const Plane = (props) => {
   return (
     <div className="plane-container">
-
+      <svg className="plane-renderer">
+        
+      </svg>
     </div>
   )
 }
