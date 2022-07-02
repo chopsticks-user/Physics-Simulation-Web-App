ef8448c657462cd032e6fe7fa0b9060d02c751af
Frost <huuquangcap@gmail.com>
2022-07-02 09:02:08 -0400

    Added zoom, pan, get current plane size.

--- src/components/Render.js
+++ src/components/Render.js
@@ -1,20 +1,30 @@
 import * as d3 from "d3"
 import { useEffect, useRef, useState } from 'react'
 
-const Render = () => {
+const Render = (props) => {
     const ref = useRef();
+    var width, height;
     
     useEffect(() => {
         const current = ref.current;
         const plane = d3.select(current);
+        window.addEventListener("resize", () => {
+            width = current.width.animVal.value;
+            height = current.height.animVal.value;
+            console.log(width, height);
+            console.log(current.width.animVal.value, current.height.animVal.value);
+        });
         current.addEventListener("click", (e) => {
             plane.append("circle")
                 .attr("cx", e.offsetX)
                 .attr("cy", e.offsetY)
                 .attr("r", 10)
-                .attr("fill", "red")
-        }, []);
-    });
+                .attr("fill", "red");
+            
+        });
+        const zoomHandler = d3.zoom().scaleExtent(.1, 5).on("zoom", () => {console.log("zoomed")});
+        zoomHandler(plane);
+    }, []);
 
     return (
         <svg className="plane-renderer" ref={ref}>
@@ -24,29 +34,3 @@ const Render = () => {
 }
 
 export default Render;
-
-// class Render extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {isToggleOn: true};
-//         this.handleZoom = this.handleZoom.bind(this);
-//         this.svg = d3.create("svg").attr("viewBox", [0, 0, 500, 500]);
-//     }
-
-//     handleZoom(e) {
-//         this.setState(prevSate => ({
-//             isToggleOn: !prevSate.isToggleOn
-//         }));
-//     }
-
-//     render() { 
-//         return (<svg style={{backgroundColor: "black"}} 
-//                      className="plane-renderer"
-//                 >
-//                     <g transform={`translate(${700}, ${600})`} onClick={(e) => this.handleZoom(e)}>
-//                         <circle style={{fill: this.state.isToggleOn ? "red" : "gold"}} color={"gold"} r={10}/>
-//                     </g>
-//                 </svg>)
-//         // return this.svg;
-//     }
-// }
