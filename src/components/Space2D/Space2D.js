4223fb2056aca74d9c757dbadbb35471392ef7fd
Frost <huuquangcap@gmail.com>
2022-07-06 16:54:48 -0400

    Added grid

--- src/components/Space2D/Space2D.js
+++ src/components/Space2D/Space2D.js
@@ -15,39 +15,67 @@ const Space2D = ({scaleMin, scaleMax, axesColor, gridLinesColor, displayGrid, di
         const container = document.querySelector(".space-2d-container");
         const w = container.clientWidth;
         const h = container.clientHeight;
+        console.log(w, h);
         
         const camera = new THREE.OrthographicCamera(-w / 2, w / 2, h / 2, -h / 2);
         const scene = new THREE.Scene();
 
-        const geometry = new THREE.PlaneGeometry(100, 100);
-        const material = new THREE.LineBasicMaterial({color: "red" });
-        const mesh = new THREE.Mesh( geometry, material );
-        scene.add( mesh );
+        // const geometry = new THREE.PlaneGeometry(3, h);
+        // const material = new THREE.LineBasicMaterial({color: "red" });
+        // const mesh = new THREE.Mesh( geometry, material );
+        // scene.add( mesh );
+        // const llmesh = new THREE.Mesh( geometry, material );
+        // scene.add( llmesh );
 
-        const lgeometry = new THREE.PlaneGeometry(100, 100);
-        const lmaterial = new THREE.LineBasicMaterial({color: "white" });
-        const lmesh = new THREE.Mesh( lgeometry, lmaterial );
-        scene.add( lmesh );
+        // const lgeometry = new THREE.PlaneGeometry(3, h);
+        // const lmaterial = new THREE.LineBasicMaterial({color: "white" });
+        // const lmesh = new THREE.Mesh( lgeometry, lmaterial );
+        // scene.add( lmesh );
+        // lmesh.position.x = 100;
 
-        const ggeometry = new THREE.PlaneGeometry(100, 100);
-        const gmaterial = new THREE.LineBasicMaterial({color: "white" });
-        const gmesh = new THREE.Mesh( ggeometry, gmaterial );
-        scene.add( gmesh );
+        // const ggeometry = new THREE.PlaneGeometry(2, h);
+        // const gmaterial = new THREE.LineBasicMaterial({color: "white" });
+        // const gmesh = new THREE.Mesh( ggeometry, gmaterial );
+        // scene.add( gmesh );
 
-        function animation( time ) {
-            mesh.rotation.y = 1;
-            lmesh.rotation.y = 1;
+        // container.addEventListener("mousewheel", () => {
+        //     mesh.position.x += 1;
+        // });
 
-            mesh.position.x += 1;
-            lmesh.position.x -= 1;
-            
-            if (mesh.position.x > w/2) {
-                mesh.position.x = -w/2;
-            }
-            if (lmesh.position.x < -w/2) {
-                lmesh.position.x = w/2;
-            }
-            renderer.render( scene, camera );
+        // function animation( time ) {
+        //     mesh.rotation.y = 1;
+        //     lmesh.rotation.y = 1;
+        //     llmesh.rotation.y = 1;
+
+        //     // mesh.position.x += 1/2;
+        //     // lmesh.position.x -= 1/2;
+
+        //     if (mesh.position.x > w/2) {
+        //         mesh.position.x = -w/2;
+        //     }
+        //     if (lmesh.position.x < -w/2) {
+        //         lmesh.position.x = w/2;
+        //     }
+        //     renderer.render( scene, camera );
+        // }
+        
+        const gridSize = 50;
+        
+        for (let i = 0; i < w / gridSize; i++) {
+            const newVerticalGridLine = new THREE.Mesh(new THREE.PlaneGeometry(i === 0 ? 4 : 2, h), new THREE.LineBasicMaterial({color: i === 0 ? axesColor : gridLinesColor}));
+            newVerticalGridLine.position.x = -w / 2 + 0 + i * gridSize;
+            newVerticalGridLine.rotation.y = 1;
+            scene.add(newVerticalGridLine);
+        }
+        for (let i = 0; i < h / gridSize; i++) {
+            const newHorizontalGridLine = new THREE.Mesh(new THREE.PlaneGeometry(w, i === 0 ? 4 : 2), new THREE.LineBasicMaterial({color: i === 0 ? axesColor : gridLinesColor}));
+            newHorizontalGridLine.position.y = -h / 2 + 1 + i * gridSize;
+            newHorizontalGridLine.rotation.x = 1;
+            scene.add(newHorizontalGridLine);
+        }
+
+        const animation = () => {
+            renderer.render(scene, camera);
         }
 
         const renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -55,7 +83,7 @@ const Space2D = ({scaleMin, scaleMax, axesColor, gridLinesColor, displayGrid, di
         renderer.setAnimationLoop( animation );
         container.append( renderer.domElement );
         renderer.render(scene, camera);
-    }, []);
+    }, [axesColor, gridLinesColor]);
 
     return (
         <div className="space-2d-container"></div>
